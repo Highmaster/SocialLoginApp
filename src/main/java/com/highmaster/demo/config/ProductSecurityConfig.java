b7aa829f57b0490f2226e84cbfde399043d93e95
Highmaster <nzechideraann@gmail.com>
2024-07-30 21:55:59 +0100

    first commit

---
+++ src/main/java/com/highmaster/demo/config/ProductSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.highmaster.demo.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class ProductSecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth ->{
+            auth.requestMatchers("/products")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();})
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults())
+                .build();
+    }
+}
---
+++ src/test/java/com/highmaster/demo/SbSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highmaster.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
